ab015c206283725072511fd3af3a8101b68503e5
Irma Rasikeviciute <rasikeviciute@yahoo.com>
2017-05-27 22:04:10 +0100

    Add eat method to animal class

--- animals/Animal.java
+++ animals/Animal.java
@@ -1,15 +1,19 @@
 package animals;
+import java.util.ArrayList;
+import food.*;
 
 public abstract class Animal {
   private String name;
   private int age;
   private String group;
+  private ArrayList<Edible> belly;
 
 
 public Animal(String name, int age, String group) {
   this.name = name;
   this.age = age;
   this.group = group;
+  this.belly = new ArrayList<Edible>();
 
 }
 
@@ -23,6 +27,18 @@ public int getAge() {
 
 public String getGroup() {
   return this.group;
+}
+
+public int foodCount() {
+  return this.belly.size();
+}
+
+public void eat(Edible food) {
+  this.belly.add(food);
+}
+
+public void sleep() {
+  this.belly.clear();
 }
 
 
--- specs/AnimalTest.java
+++ specs/AnimalTest.java
@@ -1,13 +1,22 @@
 import org.junit.*;
 import static org.junit.Assert.assertEquals;
 import animals.*;
+import food.*;
 
 public class AnimalTest {
 
 @Test
-public void testAnimalHasName() {
+public void animalHasName() {
   Eagle eagle = new Eagle("Billy", 4, "Bird");
   assertEquals("Billy", eagle.getName());
+}
+
+@Test
+public void canEatPlant() {
+  Koala koala = new Koala("Sarah", 1, "Mammal");
+  Plant bamboo = new Plant();
+  koala.eat(bamboo);
+  assertEquals(1, koala.foodCount());
 }
 
 
